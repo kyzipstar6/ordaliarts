6ced4ffb279ffddc2b51de1af6428791aa0e1fe4
kyzipstar6 <georgemichaelszup@gmail.com>
2025-07-11 08:53:55 +0200

    Update wsimbcn.js

--- wsim/js/wsimbcn.js
+++ wsim/js/wsimbcn.js
@@ -18,8 +18,8 @@ let UPPER_BOUND = ran3*37;let LOWER_BOUND = ran3*10;
                
             document.getElementById("tmp").innerText = `Temperature: ${temp.toFixed(1)} °C`;  
             document.getElementById("hum").innerText = `Humidity: ${hum.toFixed(1)} %`;  
-          if(minute<10)  document.getElementById("hour").innerText = `Hour: ${hour}:1${minute}`;  
-	       if(minute>p)  document.getElementById("hour").innerText = `Hour: ${hour}:${minute}`; 
+          if(minute<10)  document.getElementById("hour").innerText = `Hour: ${hour}:0${minute}`;  
+	       if(minute>9)  document.getElementById("hour").innerText = `Hour: ${hour}:${minute}`; 
             document.getElementById("day").innerText = `Date: ${day}/${month}/${year}`;  
             document.getElementById("wind").innerText = `Wind speed: ${quickwind.toFixed(1)} km/h`;  
             document.getElementById("pres").innerText = `Pressure: ${pressure.toFixed(1)} hPa`;  
