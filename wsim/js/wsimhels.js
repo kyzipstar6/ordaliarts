894e6d695f008e05097d0d6c1f843f687b50b0b5
kyzipstar6 <georgemichaelszup@gmail.com>
2025-07-12 17:29:44 +0200

    Update wsimhels.js

--- wsim/js/wsimhels.js
+++ wsim/js/wsimhels.js
@@ -147,11 +147,11 @@ if (document.getElementById("yei").value!=""){year = parseFloat(document.getElem
 if (document.getElementById("titi").value!=""){
 title = document.getElementById("titi").value;
 }
-  let aci = acumulator/36000;
+ 
   
 }
 function update(){
-    if(hour>19 || hour<7) { 
+    if(hour>19 || hour<7) {  let aci = acumulator/36000;
       temp-=1*aci; hum+=8*aci; pressure += 0.87*aci;}
     if(hour<20 || hour>7) { 
       temp+=1*aci; hum-=8*aci; pressure -= 0.87*aci;}
