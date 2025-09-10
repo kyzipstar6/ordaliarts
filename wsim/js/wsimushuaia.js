60ade1eea64eb6551cb90e312daa291f0110d298
kyzipstar6 <georgemichaelszup@gmail.com>
2025-09-10 02:09:02 +0200

    Update wsimushuaia.js

--- wsim/js/wsimushuaia.js
+++ wsim/js/wsimushuaia.js
@@ -112,13 +112,13 @@ if (hum>100) hum =89;
            
     }  let acumulator =0;
     let wspm = 1 + (-0.5 + Math.random()); let wspd = 34; let wspdd=7;
-    let wspm =87;
+    let wspM =87;
 let vbl= 0;
     function wind(){  
 
         setInterval(() => {  
       wspm = 1 + (-0.5 + Math.random())/wspdd;
-          if (wspd>wspm)wspm = 1 + (-0.4 + Math.random())/wspdd;
+          if (wspd>wspM)wspm = 1 + (-0.4 + Math.random())/wspdd;
           if (wspd<0.1) wspd = 5;
           wspd*=wspm;
           if (hour >0 && hour<7 || hour >22) wspm =65;
