2cb83178b626cec5ac8fbc309d8f3f9b744b21ee
kyzipstar6 <georgemichaelszup@gmail.com>
2025-10-06 22:57:05 +0200

    Update wsimordovician.js

--- wsim/js/wsimordovician.js
+++ wsim/js/wsimordovician.js
@@ -51,12 +51,12 @@ const windT = ['Do not let the wind blow over 50 km/h today'];
 let inc1 =0.1; let inc2=0.005; let inc3= inc1*50; let inc4 =inc2*50;   
     inc1 = inc1*2; inc2 = inc2*2;  
 let ran = Math.random();let ran2 = Math.random();let ran3 = Math.random();let ran4 = Math.random();let ran5 = Math.random();  
-        let hum = 77*(0.5+ (Math.random()-0.5)); let temp = 20;  
+        let hum = 77*(0.5+ (Math.random()-0.5)); let temp = 32.5;  
         let hour = Math.ceil(ran*24);  
         let minute = Math.ceil(ran2*60);  
         let day = Math.ceil(ran3*31);  
         let month = Math.ceil(ran4*12);  
-        let year = Math.ceil(ran5*4500);  let chd=0;
+        let year = 4.3245213e08;  let chd=0;
     function main() {  
 		
         if (hour > 10 && hour<18){ if(month ==6 || month==7 ){ temp=22;} if(month ==5 || month==8){ temp=24;}if(month ==4 || month==9){ temp=25.5;}  
@@ -75,7 +75,7 @@ let ran = Math.random();let ran2 = Math.random();let ran3 = Math.random();let ra
           if(minute<10)  document.getElementById("hour").innerText = `Hour: ${hour}:0${minute}`;  
 	       if(minute>9)  document.getElementById("hour").innerText = `Hour: ${hour}:${minute}`; 
             document.getElementById("day").innerText = `Date: ${day}/${month}/${year} `;  
-            document.getElementById("wind").innerText = `Wind speed: ${wspd.toFixed(1)} km/h`;  
+            document.getElementById("wspdVal").innerText = `Wind speed: ${wspd.toFixed(1)} km/h`;  
             
             document.getElementById("pressure").innerText = `Pressure: ${pressure.toFixed(1)} hPa`; 
             pillTmp.innerText = `${temp.toFixed(1)} °C`;  
@@ -106,12 +106,7 @@ if (hum>100) hum =89;
         if(month ==3 || month==10){ temp=29.7;}if(month ==2 || month==11){ temp=27.5;}if(month ==1 || month==12){ temp=26.2;}}  
         if (hour < 10 || hour>18){  if(month ==6 || month==7 ){temp=29.4;}if(month ==5 || month==8){ temp=28.5;} if(month ==4 || month==9){ temp=24.3;}  
         if(month ==3 || month==10){ temp=22.1;}if(month ==2 || month==11){ temp=20.4;}if(month ==1 || month==12){ temp=18.7;}}  
-        if (year > -1 && year <1000){temp = temp-2;}if (year > 1000 && year <1300){temp = temp-4;} if (year > 1300 && year <1700) { temp = temp - 2; }  if (year > 1600 && year <1700){temp = temp-3;}  
-        if (year > 1700 && year <1750){temp = temp-6;}if (year > 1750 && year <1920){temp = temp-5;}if (year > 1920 && year <1950){temp = temp-4;}if (year > 1950 && year <1980){temp = temp-3;}  
-        if (year > 1980 && year <2000){temp = temp-2;}if (year > 2040 && year <2100){temp = temp+2;}if (year > 2100 && year <2200){temp = temp+4;}if (year > 2200 && year <2300){temp = temp+6;}  
-        if (year > 2300 && year <2400){temp = temp+7;}if (year > 2400 && year <2600){temp = temp+6;}if (year > 2600 && year <3100){temp = temp+5;}  
-        if (year > 3100 && year <4000){temp = temp+3;}if (year > 4000 && year <4500){temp = temp-1;}  
-        setInterval(() => {  
+               setInterval(() => {  
            
             if (hour >9 && hour<14){  
             temp +=0.1;
@@ -131,8 +126,8 @@ if (hum>100) hum =89;
 			if(trend ==1&&chd==0) temp+=0.2;
 			if(trend==0) temp +=0;
 			
-			if(trend ==-1&&chd==1) hum-=0.2; 
-			if(trend ==1&&chd==1) hum+=0.2;
+			if(trend ==-1&&chd==1) hum-=1; 
+			if(trend ==1&&chd==1) hum+=1;
 			if(trend==0&&chd==1) hum +=0;
         }, 60000);  
            
